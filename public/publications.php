c88076f1f89c48d0885c70a1381f41189b8190b2
Per Olofsson <pelle@pqz.se>
2015-09-08 14:45:12 +0200

    work on publications

--- public/publications.php
+++ public/publications.php
@@ -3,7 +3,72 @@ require '../common.php';
 
 use DsvSu\Daisy;
 
+function weed_out(&$array, $key)
+{
+    if (isset($array[$key])) {
+        $array[$key] = array_filter($array[$key], function ($val) {
+                error_log(var_export($val, true));
+                return !empty($val);
+            });
+    }
+}
+
+$params = filter_input_array(
+    INPUT_GET,
+    [
+        'title' => FILTER_DEFAULT,
+        'lastName' => FILTER_DEFAULT,
+        'firstName' => FILTER_DEFAULT,
+        'type' => [
+            'filter' => FILTER_DEFAULT,
+            'flags' => FILTER_REQUIRE_ARRAY,
+        ],
+        'researchArea' => [
+            'filter' => FILTER_VALIDATE_INT,
+            'flags' => FILTER_REQUIRE_ARRAY | FILTER_NULL_ON_FAILURE,
+        ],
+        'from' => [
+            'filter' => FILTER_VALIDATE_INT,
+            'flags' => FILTER_NULL_ON_FAILURE,
+        ],
+        'until' => [
+            'filter' => FILTER_VALIDATE_INT,
+            'flags' => FILTER_NULL_ON_FAILURE,
+        ],
+    ]
+);
+
+if (empty($params)) {
+    $params = [];
+}
+
+weed_out($params, 'type');
+weed_out($params, 'researchArea');
+
+$commit = filter_input(INPUT_GET, 'commit', FILTER_VALIDATE_BOOLEAN);
+
+if ($commit) {
+    error_log(var_export($params, true));
+    $publications = Daisy\Publication::find($params);
+} else {
+    $publications = null;
+}
+
+$twig = getTwigEnv();
+
+$twig->addFilter(new Twig_SimpleFilter('format_publication', function ($pub) {
+            return $pub->getTitle();
+        }));
+
 $twig->display(
     'publications.html.twig',
-    []
+    $params + [
+        'commit' => $commit,
+        'allPublicationTypes' => Daisy\PublicationType::getAll(),
+        'allResearchAreas' => Daisy\ResearchArea::getAll(),
+        'publications' => $publications,
+        'total_count' => count($publications),
+        'first_item' => 1,
+        'last_item' => count($publications),
+    ]
 );
