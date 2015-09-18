c28a92492f53590eb6beeb0853294b8e4dec63d4
Per Olofsson <pelle@pqz.se>
2015-09-18 15:22:50 +0200

    Swedish sorting in staff table

--- js/footable-init.js
+++ js/footable-init.js
@@ -1,1 +1,11 @@
-$('.footable').footable();
+function dsvInitFootable() {
+    $('.footable').footable({
+        parsers: {
+            swedish: function (cell) {
+                return $.trim($(cell).text().replace('w', 'v').replace('W', 'V'));
+            }
+        }
+    });
+}
+
+dsvInitFootable();
